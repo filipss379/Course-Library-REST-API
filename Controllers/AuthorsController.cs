6f14b1d890be01750b1f7622df29a05e8531d6ae
Filip Stasiński <filipss379@gmail.com>
2020-01-28 19:39:55 +0100

    searching and filtering authors added

--- Controllers/AuthorsController.cs
+++ Controllers/AuthorsController.cs
@@ -28,9 +28,11 @@ namespace RESTful_API.Controllers
 
         [HttpGet()]
         [HttpHead]
-        public ActionResult<IEnumerable<AuthorDto>> GetAuthors()
+        public ActionResult<IEnumerable<AuthorDto>> GetAuthors(
+            string mainCategory,
+            string searchQuery)
         {
-            var authorsFromRepo = _courseLibraryRepository.GetAuthors();            
+            var authorsFromRepo = _courseLibraryRepository.GetAuthors(mainCategory, searchQuery);            
             return Ok(_mapper.Map<IEnumerable<AuthorDto>>(authorsFromRepo)); 
         }
 
